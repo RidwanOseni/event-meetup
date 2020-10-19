4b4a932dd39ee6d0ed33fab39d4f17d26b0a9a17
Ridwan Oseni <ridwanoseni101@gmail.com>
2020-10-19 13:39:54 +0100

    toolbar in progress

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,56 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+<v-card>
+  <!-- <v-app> -->
+    
+    <v-toolbar>
+      
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-app-bar>
+      <v-app-bar-nav-icon @click.native="sideNav = !sideNav"></v-app-bar-nav-icon>
+      <v-toolbar-title> RidMeetUp</v-toolbar-title>
+      <v-navigation-drawer v-model="sideNav">
 
-      <v-spacer></v-spacer>
+      </v-navigation-drawer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text> 
+          <!-- <v-icon left>supervisor_account</v-icon> -->
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+      </v-app-bar>
+      
+      <!-- <v-toolbar-side-icon @click.native="sideNav = !sideNav"></v-toolbar-side-icon> -->
+      
+      
+    </v-toolbar>
 
     <v-main>
-      <HelloWorld/>
+  
     </v-main>
-  </v-app>
+  <!-- </v-app> -->
+</v-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
-  name: 'App',
+  data: function () {
+                return {
+                  sideNav: false
+                }
+               
+            },
 
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  
 };
 </script>
+
+<style lang="scss" scoped>
+.v-toolbar__content, .v-toolbar__extension {
+    padding: 0px; 
+}
+
+</style>
